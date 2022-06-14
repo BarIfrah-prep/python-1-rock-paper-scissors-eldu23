e2e8086414ce8cf5050f0fec4c93d59834eabeb3
eldu <eldumb@edu.hac.ac.il>
2022-06-14 22:11:51 +0300

    hey bar i coudn't run it didn't check it if the code is good i am sending you without checking it

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -55,3 +55,65 @@ while True:
       
       """
     pass
+
+import Random
+
+#Here you write the code
+
+gameIson = True
+
+#printing message to the player
+
+userName = input("who are you ?")
+print(f"Hello welcome{userName}")
+
+while gameIson:
+    userChoice = input("choose your Choice Rock(1),Paper(2),scissors(3)")
+
+    #validating User
+    while userChoice != "1" and userChoice !="2" and userChoice != "3":
+        print("Invalid Choice")
+        userChoice = input("Choose your choice Rock(1),Paper(2),Scissors(3)")
+
+    else:
+        computerChoice = random.randint(1,3)
+        userChoice =int(userChoice)
+
+        # Case User Choose Rock
+        if userChoice == 1 and computerChoice == 3:
+            print("congra you won")
+        elif userChoice ==1 and computerChoice == 2:
+            print("oops you lose")
+        elif userChoice == 1 and computerChoice == 1:
+            print("its amazing!")
+
+        #case user choose paper
+        if userChoice == 2 and computerChoice == 1:
+            print("congra you won")
+        elif userChoice == 2 and computerChoice == 3:
+            print("oops you lose")
+        elif userChoice == 2 and computerChoice == 2:
+            print("its amazing!")
+
+            #case user choose scissors
+
+        if userChoice == 3 and computerChoice == 2:
+            print("congra you won")
+        elif userChoice == 3and computerChoice == 1:
+            print("oops you lose")
+        elif userChoice== 3and computerChoice == 3:
+            print("its amazing!")
+
+     #asking the user if he wants to continue
+
+    answerRequest = input("you want to continue? y/n")
+
+    while answerRequest !="y" and answerRequest != "n":
+        print("Invalid Answer!")
+        answerRequest = input("wanna continue ? y/n")
+
+    if answerRequest.lower() == "y":
+        continue
+    elif answerRequest.lower() == "n":
+        print("Thanks  for playing the game!")
+        gameIson = False
